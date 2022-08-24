a43a205e3b6eed1a479d5dcd67e6cd043c589cf0
Andrew Redden <andrewredden@Andrews-MacBook-Pro.local>
2022-08-24 17:50:08 +0200

    quick updates, fixes for the erc721, and auction house removed queues

--- src/AuctionHouse.sol
+++ src/AuctionHouse.sol
@@ -31,34 +31,8 @@ contract AuctionHouse is Auth, IAuctionHouse {
     ILienToken LIEN_TOKEN;
     ICollateralToken COLLATERAL_TOKEN;
 
-    // tokenContract(of the collection) => list of auctions
-    mapping(address => uint256[]) auctionQueue;
-
     // A mapping of all of the auctions currently running.
     mapping(uint256 => IAuctionHouse.Auction) auctions;
-
-    /**
-     * @notice Require that the specified auction exists
-     */
-    modifier auctionQueued(uint256 auctionId, bool auctionClose) {
-        require(auctionExists(auctionId), "Auction doesn't exist");
-        (address underlying,) = COLLATERAL_TOKEN.getUnderlying(auctionId);
-        uint256 maxActive =
-            auctionQueue[underlying].length > uint256(maxActiveAuctionsPerUnderlying)
-            ? uint256(maxActiveAuctionsPerUnderlying)
-            : auctionQueue[underlying].length;
-        bool found = false;
-        for (uint256 i = 0; i < maxActive; ++i) {
-            if (auctionQueue[underlying][i] == auctionId) {
-                found = true;
-                if (auctionClose) {
-                    delete auctionQueue[underlying][i];
-                }
-            }
-        }
-        require(found, "Auction has not started");
-        _;
-    }
 
     /*
      * Constructor
@@ -69,9 +43,7 @@ contract AuctionHouse is Auth, IAuctionHouse {
         address COLLATERAL_TOKEN_,
         address LIEN_TOKEN_,
         address transferProxy_
-    )
-        Auth(msg.sender, Authority(address(AUTHORITY_)))
-    {
+    ) Auth(msg.sender, Authority(address(AUTHORITY_))) {
         weth = weth_;
         TRANSFER_PROXY = ITransferProxy(transferProxy_);
         COLLATERAL_TOKEN = ICollateralToken(COLLATERAL_TOKEN_);
@@ -85,7 +57,10 @@ contract AuctionHouse is Auth, IAuctionHouse {
         ERC20(weth).safeApprove(address(LIEN_TOKEN), type(uint256).max);
     }
 
-    function setMaxActiveAuctionsPerUnderlying(uint8 newMax) external requiresAuth {
+    function setMaxActiveAuctionsPerUnderlying(uint8 newMax)
+        external
+        requiresAuth
+    {
         maxActiveAuctionsPerUnderlying = newMax;
     }
 
@@ -94,13 +69,15 @@ contract AuctionHouse is Auth, IAuctionHouse {
      * @dev Store the auction details in the auctions mapping and emit an AuctionCreated event.
      * If there is no curator, or if the curator is the auction creator, automatically approve the auction.
      */
-    function createAuction(uint256 tokenId, uint256 duration, address initiator, uint256 initiatorFee, uint256 epochCap)
-        external
-        requiresAuth
-        returns (uint256 reserve)
-    {
+    function createAuction(
+        uint256 tokenId,
+        uint256 duration,
+        address initiator,
+        uint256 initiatorFee,
+        uint256 epochCap
+    ) external requiresAuth returns (uint256 reserve) {
         uint256[] memory amounts;
-        (reserve, amounts,) = LIEN_TOKEN.stopLiens(tokenId);
+        (reserve, amounts, ) = LIEN_TOKEN.stopLiens(tokenId);
 
         Auction storage newAuction = auctions[tokenId];
         newAuction.duration = uint64(duration);
@@ -108,11 +85,9 @@ contract AuctionHouse is Auth, IAuctionHouse {
         newAuction.amounts = amounts;
         newAuction.initiator = initiator;
         newAuction.initiatorFee = initiatorFee;
+        newAuction.firstBidTime = uint64(block.timestamp);
+        newAuction.currentBid = 0;
         newAuction.epochCap = epochCap;
-
-        (address underlying,) = COLLATERAL_TOKEN.getUnderlying(tokenId);
-
-        auctionQueue[underlying].push(tokenId);
 
         emit AuctionCreated(tokenId, duration, reserve);
     }
@@ -123,14 +98,19 @@ contract AuctionHouse is Auth, IAuctionHouse {
      * If the auction is run in native ETH, the ETH is wrapped so it can be identically to other
      * auction currencies in this contract.
      */
-    function createBid(uint256 tokenId, uint256 amount) external override auctionQueued(tokenId, false) {
+    function createBid(uint256 tokenId, uint256 amount) external override {
         address lastBidder = auctions[tokenId].bidder;
         require(
-            auctions[tokenId].firstBidTime == 0 || block.timestamp < auctions[tokenId].firstBidTime + auctions[tokenId].duration,
+            auctions[tokenId].firstBidTime == 0 ||
+                block.timestamp <
+                auctions[tokenId].firstBidTime + auctions[tokenId].duration,
             "Auction expired"
         );
         require(
-            amount >= auctions[tokenId].currentBid + ((auctions[tokenId].currentBid * minBidIncrementPercentage) / 100),
+            amount >=
+                auctions[tokenId].currentBid +
+                    ((auctions[tokenId].currentBid *
+                        minBidIncrementPercentage) / 100),
             "Must send more than last bid by minBidIncrementPercentage amount"
         );
 
@@ -155,8 +135,12 @@ contract AuctionHouse is Auth, IAuctionHouse {
         // we want to know by how much the timestamp is less than start + duration
         // if the difference is less than the timeBuffer, increase the duration by the timeBuffer
         if (
-            (auctions[tokenId].epochCap == 0 || block.timestamp + timeBuffer < auctions[tokenId].epochCap)
-                && auctions[tokenId].firstBidTime + auctions[tokenId].duration - block.timestamp < timeBuffer
+            (auctions[tokenId].epochCap == 0 ||
+                block.timestamp + timeBuffer < auctions[tokenId].epochCap) &&
+            auctions[tokenId].firstBidTime +
+                auctions[tokenId].duration -
+                block.timestamp <
+            timeBuffer
         ) {
             // Playing code golf for gas optimization:
             // uint256 expectedEnd = auctions[auctionId].firstBidTime.add(auctions[auctionId].duration);
@@ -164,8 +148,13 @@ contract AuctionHouse is Auth, IAuctionHouse {
             // uint256 timeToAdd = timeBuffer.sub(timeRemaining);
             // uint256 newDuration = auctions[auctionId].duration.add(timeToAdd);
             uint256 oldDuration = auctions[tokenId].duration;
-            auctions[tokenId].duration =
-                uint64(oldDuration + (timeBuffer - auctions[tokenId].firstBidTime + oldDuration - block.timestamp));
+            auctions[tokenId].duration = uint64(
+                oldDuration +
+                    (timeBuffer -
+                        auctions[tokenId].firstBidTime +
+                        oldDuration -
+                        block.timestamp)
+            );
             extended = true;
         }
 
@@ -175,7 +164,7 @@ contract AuctionHouse is Auth, IAuctionHouse {
             amount,
             lastBidder == address(0), // firstBid boolean
             extended
-            );
+        );
 
         if (extended) {
             emit AuctionDurationExtended(tokenId, auctions[tokenId].duration);
@@ -191,17 +180,26 @@ contract AuctionHouse is Auth, IAuctionHouse {
         external
         override
         requiresAuth
-        auctionQueued(auctionId, true)
         returns (address winner)
     {
-        require(uint256(auctions[auctionId].firstBidTime) != 0, "Auction hasn't begun");
         require(
-            block.timestamp >= auctions[auctionId].firstBidTime + auctions[auctionId].duration, "Auction hasn't completed"
+            block.timestamp >=
+                auctions[auctionId].firstBidTime + auctions[auctionId].duration,
+            "Auction hasn't completed"
         );
         Auction storage auction = auctions[auctionId];
-        winner = auction.bidder;
+        if (auction.bidder == address(0)) {
+            winner = auction.initiator;
+        } else {
+            winner = auction.bidder;
+        }
 
-        emit AuctionEnded(auctionId, auction.bidder, auction.currentBid, auction.recipients);
+        emit AuctionEnded(
+            auctionId,
+            auction.bidder,
+            auction.currentBid,
+            auction.recipients
+        );
         LIEN_TOKEN.removeLiens(auctionId);
         delete auctions[auctionId];
     }
@@ -212,41 +210,70 @@ contract AuctionHouse is Auth, IAuctionHouse {
      */
     function cancelAuction(uint256 auctionId, address canceledBy)
         external
-        auctionQueued(auctionId, true)
         requiresAuth
     {
         require(
             auctions[auctionId].currentBid < auctions[auctionId].reservePrice,
             "cancelAuction: Auction is at or above reserve"
         );
-        _handleIncomingPayment(auctionId, auctions[auctionId].reservePrice, canceledBy);
+        _handleIncomingPayment(
+            auctionId,
+            auctions[auctionId].reservePrice,
+            canceledBy
+        );
         _cancelAuction(auctionId);
     }
 
     function getAuctionData(uint256 _auctionId)
         public
         view
-        returns (uint256 amount, uint256 duration, uint256 firstBidTime, uint256 reservePrice, address bidder)
+        returns (
+            uint256 amount,
+            uint256 duration,
+            uint256 firstBidTime,
+            uint256 reservePrice,
+            address bidder
+        )
     {
         IAuctionHouse.Auction memory auction = auctions[_auctionId];
-        return (auction.currentBid, auction.duration, auction.firstBidTime, auction.reservePrice, auction.bidder);
+        return (
+            auction.currentBid,
+            auction.duration,
+            auction.firstBidTime,
+            auction.reservePrice,
+            auction.bidder
+        );
     }
 
     /**
      * @dev Given an amount and a currency, transfer the currency to this contract.
      */
-    function _handleIncomingPayment(uint256 tokenId, uint256 transferAmount, address payee) internal {
+    function _handleIncomingPayment(
+        uint256 tokenId,
+        uint256 transferAmount,
+        address payee
+    ) internal {
         require(transferAmount > uint256(0), "cannot send nothing");
 
         Auction storage auction = auctions[tokenId];
 
-        uint256 initiatorPayment = (transferAmount * auction.initiatorFee) / 100;
-        TRANSFER_PROXY.tokenTransferFrom(weth, payee, auction.initiator, initiatorPayment);
+        uint256 initiatorPayment = (transferAmount * auction.initiatorFee) /
+            100;
+        TRANSFER_PROXY.tokenTransferFrom(
+            weth,
+            payee,
+            auction.initiator,
+            initiatorPayment
+        );
         transferAmount -= initiatorPayment;
 
         if (auction.amounts.length > 0) {
             uint256[] memory liens = LIEN_TOKEN.getLiens(tokenId);
-            for (uint256 i = liens.length - auction.amounts.length; i < liens.length; ++i) {
+            for (
+                uint256 i = liens.length - auction.amounts.length;
+                i < liens.length;
+                ++i
+            ) {
                 uint256 payment;
                 uint256 lienId = liens[i];
 
@@ -265,7 +292,12 @@ contract AuctionHouse is Auth, IAuctionHouse {
                 }
             }
         } else {
-            TRANSFER_PROXY.tokenTransferFrom(weth, payee, COLLATERAL_TOKEN.ownerOf(tokenId), transferAmount);
+            TRANSFER_PROXY.tokenTransferFrom(
+                weth,
+                payee,
+                COLLATERAL_TOKEN.ownerOf(tokenId),
+                transferAmount
+            );
         }
     }
 
--- src/ERC721.sol
+++ src/ERC721.sol
@@ -58,32 +58,48 @@ ERC721 LOGIC
     function approve(address spender, uint256 id) external virtual {
         address owner = _ownerOf[id];
 
-        require(msg.sender == owner || isApprovedForAll[owner][msg.sender], "NOT_AUTHORIZED");
+        require(
+            msg.sender == owner || isApprovedForAll[owner][msg.sender],
+            "NOT_AUTHORIZED"
+        );
 
         getApproved[id] = spender;
 
         emit Approval(owner, spender, id);
     }
 
-    function setApprovalForAll(address operator, bool approved) external virtual {
+    function setApprovalForAll(address operator, bool approved)
+        external
+        virtual
+    {
         isApprovedForAll[msg.sender][operator] = approved;
 
         emit ApprovalForAll(msg.sender, operator, approved);
     }
 
-    function transferFrom(address from, address to, uint256 id) public override (IERC721) {
+    function transferFrom(
+        address from,
+        address to,
+        uint256 id
+    ) public override(IERC721) {
         require(from == _ownerOf[id], "WRONG_FROM");
 
         require(to != address(0), "INVALID_RECIPIENT");
 
         require(
-            msg.sender == from || isApprovedForAll[from][msg.sender] || msg.sender == getApproved[id] || msg.sender == address(this),
+            msg.sender == from ||
+                isApprovedForAll[from][msg.sender] ||
+                msg.sender == getApproved[id],
             "NOT_AUTHORIZED"
         );
         _transfer(from, to, id);
     }
 
-    function _transfer(address from, address to, uint256 id) internal {
+    function _transfer(
+        address from,
+        address to,
+        uint256 id
+    ) internal {
         // Underflow of the sender's balance is impossible because we check for
         // ownership above and the recipient's balance can't realistically overflow.
         unchecked {
@@ -99,22 +115,43 @@ ERC721 LOGIC
         emit Transfer(from, to, id);
     }
 
-    function safeTransferFrom(address from, address to, uint256 id) external virtual {
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 id
+    ) external virtual {
         transferFrom(from, to, id);
 
         require(
-            to.code.length == 0
-                || ERC721TokenReceiver(to).onERC721Received(msg.sender, from, id, "") == ERC721TokenReceiver.onERC721Received.selector,
+            to.code.length == 0 ||
+                ERC721TokenReceiver(to).onERC721Received(
+                    msg.sender,
+                    from,
+                    id,
+                    ""
+                ) ==
+                ERC721TokenReceiver.onERC721Received.selector,
             "UNSAFE_RECIPIENT"
         );
     }
 
-    function safeTransferFrom(address from, address to, uint256 id, bytes calldata data) external override (IERC721) {
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 id,
+        bytes calldata data
+    ) external override(IERC721) {
         transferFrom(from, to, id);
 
         require(
-            to.code.length == 0
-                || ERC721TokenReceiver(to).onERC721Received(msg.sender, from, id, data) == ERC721TokenReceiver.onERC721Received.selector,
+            to.code.length == 0 ||
+                ERC721TokenReceiver(to).onERC721Received(
+                    msg.sender,
+                    from,
+                    id,
+                    data
+                ) ==
+                ERC721TokenReceiver.onERC721Received.selector,
             "UNSAFE_RECIPIENT"
         );
     }
@@ -123,11 +160,15 @@ ERC721 LOGIC
 ERC165 LOGIC
 //////////////////////////////////////////////////////////////*/
 
-    function supportsInterface(bytes4 interfaceId) public view virtual returns (bool) {
-        return interfaceId == 0x01ffc9a7
-            || // ERC165 Interface ID for ERC165
-            interfaceId == 0x80ac58cd
-            || // ERC165 Interface ID for ERC721
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        returns (bool)
+    {
+        return
+            interfaceId == 0x01ffc9a7 || // ERC165 Interface ID for ERC165
+            interfaceId == 0x80ac58cd || // ERC165 Interface ID for ERC721
             interfaceId == 0x5b5e139f; // ERC165 Interface ID for ERC721Metadata
     }
 
@@ -175,20 +216,34 @@ INTERNAL SAFE MINT LOGIC
         _mint(to, id);
 
         require(
-            to.code.length == 0
-                || ERC721TokenReceiver(to).onERC721Received(msg.sender, address(0), id, "")
-                    == ERC721TokenReceiver.onERC721Received.selector,
+            to.code.length == 0 ||
+                ERC721TokenReceiver(to).onERC721Received(
+                    msg.sender,
+                    address(0),
+                    id,
+                    ""
+                ) ==
+                ERC721TokenReceiver.onERC721Received.selector,
             "UNSAFE_RECIPIENT"
         );
     }
 
-    function _safeMint(address to, uint256 id, bytes memory data) internal virtual {
+    function _safeMint(
+        address to,
+        uint256 id,
+        bytes memory data
+    ) internal virtual {
         _mint(to, id);
 
         require(
-            to.code.length == 0
-                || ERC721TokenReceiver(to).onERC721Received(msg.sender, address(0), id, data)
-                    == ERC721TokenReceiver.onERC721Received.selector,
+            to.code.length == 0 ||
+                ERC721TokenReceiver(to).onERC721Received(
+                    msg.sender,
+                    address(0),
+                    id,
+                    data
+                ) ==
+                ERC721TokenReceiver.onERC721Received.selector,
             "UNSAFE_RECIPIENT"
         );
     }
@@ -197,7 +252,12 @@ INTERNAL SAFE MINT LOGIC
 /// @notice A generic interface for a contract which properly accepts ERC721 tokens.
 /// @author Solmate (https://github.com/transmissions11/solmate/blob/main/src/tokens/ERC721.sol)
 abstract contract ERC721TokenReceiver {
-    function onERC721Received(address, address, uint256, bytes calldata) external virtual returns (bytes4) {
+    function onERC721Received(
+        address,
+        address,
+        uint256,
+        bytes calldata
+    ) external virtual returns (bytes4) {
         return ERC721TokenReceiver.onERC721Received.selector;
     }
 }
