608fc2c7f41ecd7f3ec4a5d2ed4e2011b68e141f
Andrew Redden <andrew@astaria.xyz>
2023-02-07 20:54:50 -0400

    C4:175 - remove undeed approvals

--- src/ERC4626RouterBase.sol
+++ src/ERC4626RouterBase.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity ^0.8.17;
 
-import {IERC4626} from "core/interfaces/IERC4626.sol";
+import {IERC4626} from "src/interfaces/IERC4626.sol";
 import {IERC4626RouterBase} from "gpl/interfaces/IERC4626RouterBase.sol";
 import {ERC20} from "solmate/tokens/ERC20.sol";
 import {SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
@@ -18,7 +18,6 @@ abstract contract ERC4626RouterBase is IERC4626RouterBase, Multicall {
     uint256 shares,
     uint256 maxAmountIn
   ) public payable virtual override returns (uint256 amountIn) {
-    ERC20(vault.asset()).safeApprove(address(vault), shares);
     if ((amountIn = vault.mint(shares, to)) > maxAmountIn) {
       revert MaxAmountError();
     }
@@ -44,8 +43,6 @@ abstract contract ERC4626RouterBase is IERC4626RouterBase, Multicall {
     uint256 amount,
     uint256 maxSharesOut
   ) public payable virtual override returns (uint256 sharesOut) {
-
-    ERC20(address(vault)).safeApprove(address(vault), amount);
     if ((sharesOut = vault.withdraw(amount, to, msg.sender)) > maxSharesOut) {
       revert MaxSharesError();
     }
@@ -58,8 +55,6 @@ abstract contract ERC4626RouterBase is IERC4626RouterBase, Multicall {
     uint256 shares,
     uint256 minAmountOut
   ) public payable virtual override returns (uint256 amountOut) {
-
-    ERC20(address(vault)).safeApprove(address(vault), shares);
     if ((amountOut = vault.redeem(shares, to, msg.sender)) < minAmountOut) {
       revert MinAmountError();
     }
